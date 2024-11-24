7596fe2828fa2c2483b94a0ebe3dd2cea2d696e1
whttsup <93023443+whttsup@users.noreply.github.com>
2024-11-24 13:07:25 +0700

    Init code

--- app.js
+++ app.js
@@ -5,18 +5,25 @@ const port = 3000;
 const path = require('path');
 
 // Đặt thư mục 'documents' làm thư mục tĩnh để truy cập trực tiếp các file HTML
-app.use(express.static(path.join(__dirname, 'documents')));
+app.use(express.static(path.join(__dirname, 'documents/html')));
 
 // Route cho trang chủ
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'documents', 'index.html'));
+    res.sendFile(path.join(__dirname, 'documents/html', 'index.html'));
 });
 
 // Route cho trang contact
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'documents', 'contact.html'));
+    res.sendFile(path.join(__dirname, 'documents/html', 'contact.html'));
 });
 
+app.get('/skills', (req, res) => {
+    res.sendFile(path.join(__dirname, 'documents/html', 'skills.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'documents/html', 'about.html'));
+});
 // Lắng nghe trên cổng được chỉ định
 app.listen(port, () => {
     console.log(`Server đang chạy trên cổng ${port}`);
